9be41b6ef5e3dd174b9fbc8d23eecb7d76df92f7
Daniyal Pervaiz <160670546+daniyalpervaiz@users.noreply.github.com>
2024-03-07 02:45:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+"use strict";
+//Addition
+let num = 5;
+let num1 = 7;
+console.log(num + num1);
+//Subtraction
+let num2 = 10;
+let num3 = 6;
+console.log(num2 - num3);
+//Multiplication
+let num4 = 4;
+let num5 = 5;
+console.log(num4 * num5);
+//Division
+let num6 = 20;
+let num7 = 4;
+console.log(num6 / num7);
+//modulus
+let num8 = 26;
+let num9 = 5;
+console.log(num8 % num9);
+//Exponential
+let num10 = 4;
+let num11 = 3;
+console.log(num10 ** num11);
---
+++ main.ts
@@ -0,0 +1,30 @@
+//Addition
+let num:number=5
+let num1:number=7
+console.log(num+num1);
+
+//Subtraction
+let num2:number=10
+let num3:number=6
+console.log(num2-num3);
+
+//Multiplication
+let num4:number=4
+let num5:number=5
+console.log(num4*num5);
+
+//Division
+let num6:number=20
+let num7:number=4
+console.log(num6/num7);
+
+
+//modulus
+let num8:number=26
+let num9:number=5
+console.log(num8%num9);
+
+//Exponential
+let num10:number=4
+let num11:number=3
+console.log(num10**num11);
